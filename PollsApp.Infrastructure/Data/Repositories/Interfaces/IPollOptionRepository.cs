d905c81783072701477d557f7bcc4a616cdf26d9
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-06-21 21:26:57 -0300

    Adiciona repository para PollOptions

---
+++ PollsApp.Infrastructure/Data/Repositories/Interfaces/IPollOptionRepository.cs
@@ -0,0 +1,10 @@
+﻿using PollsApp.Domain.Entities;
+
+namespace PollsApp.Infrastructure.Data.Repositories.Interfaces;
+
+public interface IPollOptionRepository : IBaseRepository<IPollOptionRepository>
+{
+    Task<PollOption> SaveAsync(PollOption pollOption);
+    Task<IEnumerable<PollOption>> SaveAsync(IEnumerable<PollOption> pollOptions);
+    Task<IEnumerable<PollOption>> GetByPollIdAsync(Guid pollId);
+}
