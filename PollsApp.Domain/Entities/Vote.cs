dccb55f049b6737127b863e3ad1abe167074b584
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-06-23 01:45:01 -0300

    Adiciona entidade de Voto (Vote)

---
+++ PollsApp.Domain/Entities/Vote.cs
@@ -0,0 +1,33 @@
+﻿namespace PollsApp.Domain.Entities;
+
+public class Vote
+{
+    public Guid Id { get; private set; }
+    public Guid PollId { get; private set; }
+    public Guid PollOptionId { get; private set; }
+    public Guid UserId { get; private set; }
+    public DateTime VotedAt { get; private set; }
+
+    public Vote(Guid id, Guid pollId, Guid pollOptionId, Guid userId, DateTime votedAt)
+    {
+        Id = id;
+        PollId = pollId;
+        PollOptionId = pollOptionId;
+        UserId = userId;
+        VotedAt = votedAt;
+    }
+
+    public Vote(Guid pollId, Guid pollOptionId, Guid userId)
+    {
+        Id = Guid.Empty;
+        PollId = pollId;
+        PollOptionId = pollOptionId;
+        UserId = userId;
+        VotedAt = DateTime.UtcNow;
+    }
+
+    public void ChangeOption(Guid newPollOptionId)
+    {
+        PollOptionId = newPollOptionId;
+    }
+}
