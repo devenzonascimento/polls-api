b5abad6ecd4e5911df9ac5831876b1bd8d8d509d
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-07-07 23:44:35 -0300

    Adiciona método para indexar uma enquete no OpenSearch ao criar ela

--- PollsApp.Application/Commands/Handlers/CreatePollCommandHandler.cs
+++ PollsApp.Application/Commands/Handlers/CreatePollCommandHandler.cs
@@ -1,22 +1,27 @@
 ﻿using MediatR;
+using PollsApp.Application.Services.Interfaces;
 using PollsApp.Domain.Entities;
 using PollsApp.Infrastructure.Data.Repositories.Interfaces;
+using PollsApp.Infrastructure.Data.Search.Documents;
 
 namespace PollsApp.Application.Commands.Handlers;
 
 public class CreatePollCommandHandler : IRequestHandler<CreatePollCommand, Guid>
 {
     private readonly IPollRepository pollRepository;
+    private readonly IPollSearchService pollSearchService;
 
-    public CreatePollCommandHandler(IPollRepository pollRepository)
+    public CreatePollCommandHandler(IPollRepository pollRepository, IPollSearchService pollSearchService)
     {
         this.pollRepository = pollRepository;
+        this.pollSearchService = pollSearchService;
     }
 
     public async Task<Guid> Handle(CreatePollCommand request, CancellationToken cancellationToken)
     {
         var poll = new Poll(request.Title, request.Description, request.UserRequesterId, request.ClosesAt);
 
+        PollDocument pollDocument;
         using (var transaction = pollRepository.StartTransaction())
         {
             try
@@ -30,6 +35,8 @@ public class CreatePollCommandHandler : IRequestHandler<CreatePollCommand, Guid>
 
                 await pollRepositoryWithTransaction.SaveAsync(options).ConfigureAwait(false);
 
+                pollDocument = new PollDocument(poll, options);
+
                 transaction.Commit();
             }
             catch (Exception e)
@@ -38,6 +45,8 @@ public class CreatePollCommandHandler : IRequestHandler<CreatePollCommand, Guid>
                 throw new InvalidOperationException("Failed to create poll and options", e);
             }
         }
+
+        await pollSearchService.IndexPollAsync(pollDocument).ConfigureAwait(false);
 
         return poll.Id;
     }
